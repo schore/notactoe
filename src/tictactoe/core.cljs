5f0199403ebde52300048e7850d4ae06bce3a1e7
schore <schore.r@gmx.de>
2019-12-29 12:49:09 +0100

    Now with some specs

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -1,5 +1,6 @@
 (ns tictactoe.core
   (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.spec.alpha :as s]
             [tictactoe.game]
             [tictactoe.reduceboard]))
 
@@ -7,34 +8,35 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
-
 (defonce my-app-state
   (atom (tictactoe.game/new-game)))
 
 (defn pc-move
   [game]
+  {:pre [(s/valid? :tictactoe.game/game game)]}
   (let [next-mv (tictactoe.game/get-next-move game)]
     (if (nil? next-mv)
       game
-      (assoc-in game (concat [:boards] next-mv) 1))))
+      (assoc-in game (concat [:tictactoe.game/boards] next-mv) 1))))
 
 (defn play-round
   [game i j k]
+  {:pre [(s/valid? :tictactoe.game/game game)]}
   (-> game
-      (assoc-in [:boards i j k] 1)
+      (assoc-in [:tictactoe.game/boards i j k] 1)
       (pc-move)))
 
 (defn blank [game i j k]
   [:rect {:width 0.9
           :height 0.9
           :fill (cond
-                  (tictactoe.board/dead? (get-in game [:boards i])) "grey"
-                  (tictactoe.reduceboard/winning? (:boards game) i j k) "green"
+                  (tictactoe.board/dead? (get-in game [:tictactoe.game/boards i])) "grey"
+                  (tictactoe.reduceboard/winning? (:tictactoe.game/boards game) i j k) "green"
                   :else "red")
           :x (+ (* 4 i) j)
           :y k
           :on-click (fn rect-click [e]
-                      (if (not (tictactoe.board/dead? (get-in game [:boards i])))
+                      (if (not (tictactoe.board/dead? (get-in game [:tictactoe.game/boards i])))
                         (swap! my-app-state #(play-round % i j k))))}])
 
 (defn cross [i j k]
@@ -55,7 +57,7 @@
 
 (defn get-scores
   [game]
-  (clojure.string/join " " (for [i (:boards game)]
+  (clojure.string/join " " (for [i (:tictactoe.game/boards game)]
                              (tictactoe.board/get-value i))))
 
 (defn tictactoe
@@ -66,15 +68,15 @@
     [:h1 "Tic Tac Toe"]
     [:p (reset-button)]
     [:p (get-scores app-state)]
-    [:p (str (tictactoe.reduceboard/calc-game (:boards app-state)))]
+    [:p (str (tictactoe.reduceboard/calc-game (:tictactoe.game/boards app-state)))]
     (into
      [:svg {:view-box (str "0 0 " 13 " " tictactoe.board/board-size)
             :witdth 400
             :height 400}
-      (for [i (range (count (:boards app-state)))
+      (for [i (range (count (:tictactoe.game/boards app-state)))
             j (range tictactoe.board/board-size)
             k (range tictactoe.board/board-size)]
-        (case (get-in app-state [:boards i j k])
+        (case (get-in app-state [:tictactoe.game/boards i j k])
           0 [blank app-state i j k]
           1 [cross i j k]))])]))
 
--- src/tictactoe/game.cljs
+++ src/tictactoe/game.cljs
@@ -1,18 +1,30 @@
 (ns tictactoe.game
   (:require [tictactoe.board]
-            [tictactoe.reduceboard]))
+            [clojure.spec.alpha :as s]))
 
 (def number-of-boards 3)
 
+(s/def ::zero-or-one #{0 1})
+(s/def ::board (s/coll-of (s/coll-of ::zero-or-one
+                                     :kind vector? :count 3)
+                          :kind vector? :count 3))
+
+(s/def ::boards (s/coll-of ::board :kind vector?))
+(s/def ::game (s/keys :req [::boards]))
+
 (defn new-game
-  ([] (new-game number-of-boards))
+  ([]
+   {:post [(s/valid? ::game %)]}
+   (new-game number-of-boards))
   ([n]
-   {:boards (into [] (for [i (range n)] tictactoe.board/new-board))
-    :player :p1}))
+   {:post [(s/valid? ::game %)]}
+   {::boards (into [] (for [i (range n)] tictactoe.board/new-board))}))
 
 (defn cross-out
   [game i j k]
-  (assoc-in game [:boards i j k] 1))
+  {:pre [(s/valid? ::game game)]
+   :post [(s/valid? ::game %)]}
+  (assoc-in game [::boards i j k] 1))
 
 (defn get-playable-boards
   [game]
@@ -25,30 +37,35 @@
 
 (defn possible-moves
   [game]
-  (let [all-moves (for [i (range (count (:boards game)))
+  {:pre [(s/valid? ::game game)]}
+  (let [all-moves (for [i (range (count (::boards game)))
                         j (range tictactoe.board/board-size)
                         k (range tictactoe.board/board-size)]
                     [i j k])]
     (->> all-moves
-         (filter #(not (tictactoe.board/dead? (get-in game [:boards (first %)]))))
-         (filter #(= 0 (get-in game (concat [:boards] %)))))))
+         (filter #(not (tictactoe.board/dead? (get-in game [::boards (first %)]))))
+         (filter #(= 0 (get-in game (concat [::boards] %)))))))
 
 (defn non-ending-moves
   [game]
-  (filter #(not (empty? (possible-moves (assoc-in game (concat [:boards] %) 1)))) (possible-moves game)))
+  {:pre [(s/valid? ::game game)]}
+  (filter #(not (empty? (possible-moves (assoc-in game (concat [::boards] %) 1)))) (possible-moves game)))
 
 (defn winning-moves
   [game]
+  {:pre [(s/valid? ::game game)]}
   (->> (possible-moves game)
-       (filter #(apply (partial tictactoe.reduceboard/winning? (:boards game)) %))))
+       (filter #(apply (partial tictactoe.reduceboard/winning? (::boards game)) %))))
 
 (defn get-next-move
   "get the next mv, choose an winning move if possible
   In case f is passed, it is possible to choose the element,
   which you wan't to use"
   ([game]
+   {:pre [(s/valid? ::game game)]}
    (get-next-move game rand-int))
   ([game f]
+   {:pre [(s/valid? ::game game)]}
    (let [winning-mv (winning-moves game)
          non-ending-mv (non-ending-moves game)
          mv (possible-moves game)]
